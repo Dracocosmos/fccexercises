d021a70babecacde4e97c0bc9162933da3f88e8c
Dracocosmos <notreal@mail.com>
2024-05-15 08:11:14 +0300

    Correct html page, working basic react setup, no redux.

--- src/App.js
+++ src/App.js
@@ -1,11 +1,18 @@
 import React from "react";
+// TODO = Make a react object that has a list of all the excercises, with links that 
+// refreshes the page to that exercise.
+
+const exercises = [
+  "random_quote_machine",
+]
 
 const App = () =>{
-    return (
-        <h1>
-            Welcome to React App thats build using Webpack and Babel separately
-        </h1>
-    )
+  return (
+    <h1>
+      { exercises.forEach() }
+      Welcome to a React App that has been built using Webpack and Babel separately
+    </h1>
+  )
 }
 
 export default App
