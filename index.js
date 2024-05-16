554f4627bea89e9c6f81435fcb459f7416225317
Dracocosmos <notreal@mail.com>
2024-05-16 11:42:05 +0300

    redux working for now

--- index.js
+++ index.js
@@ -1,5 +1,13 @@
 import React from "react";
 import reactDom from "react-dom";
+import store from "./src/store";
+
+import { Provider } from "react-redux";
+
 import App from "./src/App";
 
-reactDom.render(<App />, document.getElementById("root"));
+reactDom.render(
+  <Provider store={ store }>
+    <App />
+  </Provider>,
+  document.getElementById("root"));
