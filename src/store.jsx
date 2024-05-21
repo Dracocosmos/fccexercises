174849bb148c608d9c4685c412d667a917006560
Dracocosmos <notreal@mail.com>
2024-05-21 12:31:01 +0300

    Almost working modular setup

--- src/store.jsx
+++ src/store.jsx
@@ -1,12 +1,37 @@
 import { combineReducers, createStore } from "redux";
 
+// get from local storage
+const getActiveExercise = () => {
+  // reset
+  // setActiveExercise();
+  const storageItem = JSON.parse(sessionStorage.getItem("activeExercise"))
+  console.log(storageItem)
+  return storageItem.active
+}
+
+// set to local storage, no exercise = reset
+const setActiveExercise = (exercise = null) => {
+  // what to save in localstorage
+  const storageItem = {
+    active: exercise,
+    reset: null
+  };
+
+  sessionStorage.setItem("activeExercise", JSON.stringify(storageItem));
+
+  return storageItem.active
+};
+
+// set initial state for store
 const exerciseInitialState = {
   list: [
     "Random Quote Machine",
   ],
-  active: null,
+  // check localstorage if there is an active exercise
+  active: getActiveExercise(),
 }
 
+// reducer for switching exercise
 const exerciseReducer = (state, action) => {
 
   if (state == undefined) {
@@ -15,15 +40,23 @@ const exerciseReducer = (state, action) => {
 
   switch (action.type) {
     case 'exercises/activate':
+      setActiveExercise(action.payload);
       return {
         ...state,
-        active: action.payload
+        active: getActiveExercise()
+      };
+    case 'exercises/reset':
+      setActiveExercise()
+      return {
+        ...state,
+        active: getActiveExercise()
       };
     default:
       return state;
   };
 };
 
+// if more reducers in the future
 const rootReducer = combineReducers({
   exercises: exerciseReducer,
 });
