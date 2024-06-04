68ba8a53b2c0a2d6b7a68cec8c917bd131483390
Dracocosmos <notreal@mail.com>
2024-06-04 12:01:09 +0300

    trying to get test exercise to work

--- src/exercise template/exercise template.jsx
+++ src/exercise template/exercise template.jsx
@@ -1,5 +1,7 @@
 import ReactDOM from "react-dom";
 import React from "react";
+
+import { createStore } from "redux";
 import { Provider } from "react-redux";
 
 import $ from "jquery";
@@ -9,8 +11,24 @@ import "../../Public/exercise template/exercise template.css"
 // for resetting to the exercise menu
 import Reset from "../Reset";
 
+const storeInitial = {
+};
+
+const exampleReducer = (state = storeInitial, action) => {
+  switch (action.type) {
+    case 'default/get':
+      return {
+        ...state,
+      };
+    default:
+      return state;
+  };
+};
+
+const store = createStore(exampleReducer);
+
 ReactDOM.render(
-  <Provider store={""}>
+  <Provider store={store}>
     <Reset></Reset>
     template
   </Provider>,
--- src/test/test.jsx
+++ src/test/test.jsx
@@ -1,16 +1,34 @@
 import ReactDOM from "react-dom";
 import React from "react";
+
+import { createStore } from "redux";
 import { Provider } from "react-redux";
 
 import $ from "jquery";
 
 // for hot loading css
-import "../../Public/test/test.css"
+import "../../Public/Test/Test.css"
 // for resetting to the exercise menu
 import Reset from "../Reset";
 
+const storeInitial = {
+};
+
+const exampleReducer = (state = storeInitial, action) => {
+  switch (action.type) {
+    case 'default/get':
+      return {
+        ...state,
+      };
+    default:
+      return state;
+  };
+};
+
+const store = createStore(exampleReducer);
+
 ReactDOM.render(
-  <Provider store={""}>
+  <Provider store={store}>
     <Reset></Reset>
     template
   </Provider>,
