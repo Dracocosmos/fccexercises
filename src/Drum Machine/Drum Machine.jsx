39cd71fcb7824a82aaf68b6a11461fa71065c502
Dracocosmos <notreal@mail.com>
2024-06-19 11:29:05 +0300

    Drum Machine trying to get Indexed DB working

--- src/Drum Machine/Drum Machine.jsx
+++ src/Drum Machine/Drum Machine.jsx
@@ -11,48 +11,158 @@ import "../../Public/Drum Machine/Drum Machine.css"
 // for resetting to the exercise menu
 import Reset from "../Reset";
 
+const sampleSources = [
+  {
+    name: 'Heater 1',
+    url: 'https://cdn.freecodecamp.org/testable-projects-fcc/audio/Heater-1.mp3'
+  },
+  {
+    name: 'Heater 2',
+    url: 'https://cdn.freecodecamp.org/testable-projects-fcc/audio/Heater-2.mp3'
+  },
+  {
+    name: 'Heater 3',
+    url: 'https://cdn.freecodecamp.org/testable-projects-fcc/audio/Heater-3.mp3'
+  },
+  {
+    name: 'Heater 4',
+    url: 'https://cdn.freecodecamp.org/testable-projects-fcc/audio/Heater-4_1.mp3'
+  },
+  {
+    name: 'Clap',
+    url: 'https://cdn.freecodecamp.org/testable-projects-fcc/audio/Heater-6.mp3'
+  },
+  {
+    name: 'Open HH',
+    url: 'https://cdn.freecodecamp.org/testable-projects-fcc/audio/Dsc_Oh.mp3'
+  },
+  {
+    name: 'Kick-n\'-hat',
+    url: 'https://cdn.freecodecamp.org/testable-projects-fcc/audio/Kick_n_Hat.mp3'
+  },
+  {
+    name: 'Kick',
+    url: 'https://cdn.freecodecamp.org/testable-projects-fcc/audio/RP4_KICK_1.mp3'
+  },
+  {
+    name: 'Closed-HH',
+    url: 'https://cdn.freecodecamp.org/testable-projects-fcc/audio/Cev_H2.mp3'
+  }
+];
+
 const storeInitial = {
   samples: [
     {
-      key: 'Q', name: 'Heater 1', reactKey: 1,
-      url: 'https://cdn.freecodecamp.org/testable-projects-fcc/audio/Heater-1.mp3'
+      key: 'Q', name: '', reactKey: 1,
+      url: ''
     },
     {
-      key: 'W', name: 'Heater 2', reactKey: 2,
-      url: 'https://cdn.freecodecamp.org/testable-projects-fcc/audio/Heater-2.mp3'
+      key: 'W', name: '', reactKey: 2,
+      url: ''
     },
     {
-      key: 'E', name: 'Heater 3', reactKey: 3,
-      url: 'https://cdn.freecodecamp.org/testable-projects-fcc/audio/Heater-3.mp3'
+      key: 'E', name: '', reactKey: 3,
+      url: ''
     },
     {
-      key: 'A', name: 'Heater 4', reactKey: 4,
-      url: 'https://cdn.freecodecamp.org/testable-projects-fcc/audio/Heater-4_1.mp3'
+      key: 'A', name: '', reactKey: 4,
+      url: ''
     },
     {
-      key: 'S', name: 'Clap', reactKey: 5,
-      url: 'https://cdn.freecodecamp.org/testable-projects-fcc/audio/Heater-6.mp3'
+      key: 'S', name: '', reactKey: 5,
+      url: ''
     },
     {
-      key: 'D', name: 'Open HH', reactKey: 6,
-      url: 'https://cdn.freecodecamp.org/testable-projects-fcc/audio/Dsc_Oh.mp3'
+      key: 'D', name: '', reactKey: 6,
+      url: ''
     },
     {
-      key: 'Z', name: 'Kick-n\'-hat', reactKey: 7,
-      url: 'https://cdn.freecodecamp.org/testable-projects-fcc/audio/Kick_n_Hat.mp3'
+      key: 'Z', name: '', reactKey: 7,
+      url: ''
     },
     {
-      key: 'X', name: 'Kick', reactKey: 8,
-      url: 'https://cdn.freecodecamp.org/testable-projects-fcc/audio/RP4_KICK_1.mp3'
+      key: 'X', name: '', reactKey: 8,
+      url: ''
     },
     {
-      key: 'C', name: 'Closed-HH', reactKey: 9,
-      url: 'https://cdn.freecodecamp.org/testable-projects-fcc/audio/Cev_H2.mp3'
+      key: 'C', name: '', reactKey: 9,
+      url: ''
     }
   ],
   lastPlayed: null,
 };
 
+// delete old database
+try {
+  indexedDB.deleteDatabase('SampleDatabase')
+} catch (error) {
+  console.log("old database not found, no action needed");
+}
+
+// needs to be a promise, so the site waits for the db creation
+// there is no error handling, oops
+const createDb = () => {
+  return new Promise((resolve, reject) => {
+
+    // open db, sends an event, upgradeneeded
+    const request = window.indexedDB.open("SampleDatabase");
+
+    request.onerror = (event) => {
+      console.error("database request failed");
+      console.error(event)
+      reject(event)
+    };
+
+    request.onsuccess = (event) => {
+
+      const sampledb = event.target.result;
+      console.log("sample database created")
+
+      resolve(sampledb)
+    };
+
+    // handle opening request
+    request.onupgradeneeded = (event) => {
+      const db = event.target.result;
+      console.log(`Upgrading to version ${db.version}`);
+
+      // Create an objectStore for this database
+      const objectStore = db.createObjectStore("SampleDatabase", {
+        keyPath: "taskTitle",
+      });
+
+      // define what data items the objectStore will contain
+      objectStore.createIndex("hours", "hours", { unique: false });
+      objectStore.createIndex("minutes", "minutes", { unique: false });
+      objectStore.createIndex("day", "day", { unique: false });
+      objectStore.createIndex("month", "month", { unique: false });
+      objectStore.createIndex("year", "year", { unique: false });
+    };
+
+    request.onblocked = (event) => {
+      console.log('blocked ...', event);
+
+      event.target.result.close();
+    };
+
+  });
+};
+
+const sampledb = await createDb()
+console.log(sampledb)
+
+sampledb.onerror = (event) => {
+  // Generic error handler for all errors targeted at this database's
+  // requests!
+  console.error(`Database error: ${event.target.errorCode}`);
+};
+
+// sets all audio to local db
+sampleSources.forEach((sample, _index) => {
+  const sampleAudio = new Audio(sample.url);
+
+
+});
 
 const audioReducer = (state = storeInitial, action) => {
   switch (action.type) {
