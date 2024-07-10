878ce7080e07cb1180a6bbfc6afd1a0e77453800
Dracocosmos <notReal@gmail.com>
2024-07-10 11:33:27 +0300

    JavaScript Calculator button push logic change

--- src/JavaScript Calculator/JavaScript Calculator.jsx
+++ src/JavaScript Calculator/JavaScript Calculator.jsx
@@ -17,39 +17,60 @@ const storeInitial = {
   currentTotal: 0,
   currentFormula: "",
   acceptableButtons: {
-    "/": "operator",
-    "*": "operator",
-    "-": "operator",
-    "+": "operator",
-    "0": "number",
-    "1": "number",
-    "2": "number",
-    "3": "number",
-    "4": "number",
-    "5": "number",
-    "6": "number",
-    "7": "number",
-    "8": "number",
-    "9": "number",
-    ".": "operator",
-    ",": "operator",
-    "Enter": "operator",
+    "0": { type: "number", id: "zero" },
+    "1": { type: "number", id: "one" },
+    "2": { type: "number", id: "two" },
+    "3": { type: "number", id: "three" },
+    "4": { type: "number", id: "four" },
+    "5": { type: "number", id: "five" },
+    "6": { type: "number", id: "six" },
+    "7": { type: "number", id: "seven" },
+    "8": { type: "number", id: "eight" },
+    "9": { type: "number", id: "nine" },
+    ".": { type: "decimal point", id: "dot" },
+    ",": { type: "alias", id: "comma", aliasFor: "." },
+    "Enter": { type: "operator", id: "enter" },
+    "/": { type: "operator", id: "division" },
+    "*": { type: "operator", id: "multiplication" },
+    "-": { type: "operator", id: "subtraction" },
+    "+": { type: "operator", id: "addition" },
   },
 };
+
 const calculatorReducer = (state = storeInitial, action) => {
   switch (action.type) {
     case 'currentFormula/update':
-      state.currentFormula[-1];
-      const newFormula = state.currentFormula + action.payload
-      const newTotal = newFormula
+      const buttons = state.acceptableButtons;
+      const currentButton = buttons[action.payload];
+      const currentFormula = state.currentFormula;
+      const lastInput = currentFormula.slice(-1);
+      const lastInputObject = buttons[lastInput];
+
+      // if action is an alias
+      if (currentButton.type === "alias") {
+        action.payload = currentButton.aliasFor;
+      };
+
+      // if action is a repeat operator
+      if (lastInput
+        && lastInputObject.type === "operator"
+        && currentButton.type === "operator"
+      ) {
+        action.payload = "";
+      };
+
+      const newFormula = state.currentFormula + action.payload;
+      const newTotal = newFormula;
+
       console.log(newFormula)
       return {
         ...state,
         currentFormula: newFormula
-      }
+      };
     case 'formula/calculate':
       return {
         ...state,
+        currentTotal: String.eval
       };
     default:
       return state;
@@ -58,68 +79,88 @@ const calculatorReducer = (state = storeInitial, action) => {
 
 const store = createStore(calculatorReducer);
 
-const Number = (props) => {
-  // the tests need the id to be written out...
-  const numberTexts = [
-    "zero",
-    "one",
-    "two",
-    "three",
-    "four",
-    "five",
-    "six",
-    "seven",
-    "eight",
-    "nine",
-  ];
+// logic for any button click
+const userInput = (key) => {
+  if (Object.keys(storeInitial.acceptableButtons)
+    // gets the first class in classlist
+    .includes(key)) {
+    if (key === "Enter") {
+      store.dispatch({
+        type: "formula/calculate"
+      })
+    } else {
+      store.dispatch({
+        type: 'currentFormula/update',
+        payload: key
+      });
+    };
+  };
+};
+
+// single button
+const Pad = (props) => {
+  // on button press
+  const handleButtonClick = (event) => {
+    event.preventDefault();
+    userInput(event.target.classList.item(0))
+  };
 
   return (
-    <button id={numberTexts[props.number]}>
-      {props.number}
-    </button>
+    <button id={props.id}
+      onClick={handleButtonClick}
+      className={`${props.button} pad-button`}
+    >
+      {props.button}
+    </button >
   )
-}
+};
 
 
 const NumberPad = () => {
-  // the ...Array etc. creates an array of numbers 0-9, which are
-  // sent one by one to a Number component
+  // Send all of the acceptable keys to create individual buttons
   return (
     <div id="">
-      {[...Array(10).keys()].map((pnumber) => {
-        return <Number {...{ number: pnumber }} key={pnumber} />
-      })}
+      {Object.entries(store.getState().acceptableButtons)
+        .map((buttonInfo, index) => {
+          // only real buttons, not their aliases
+          if (buttonInfo[1].type !== "alias") {
+            return <Pad {...{
+              button: buttonInfo[0],
+              ...buttonInfo[1]
+            }} key={index} />
+          };
+        })}
     </div>
   )
-}
+};
 
 const Calculator = (props) => {
-  window.addEventListener("keydown", (event) => {
-    if (Object.keys(storeInitial.acceptableButtons).includes(event.key)) {
-      // console.log(event)
-      store.dispatch({ type: 'currentFormula/update', payload: event.key });
-    };
-  });
+  // calculator button hits
+  const handleKeyDown = (event) => {
+    event.preventDefault();
+    userInput(event.key);
+  };
 
-  useEffect(() => {
+  React.useEffect(() => {
     // componentdidmount here
+    window.addEventListener("keydown", handleKeyDown);
     return () => {
       // componentwillunmount in functional component.
       // Anything in here is fired on component unmount.
-      window.removeEventListener("keydown")
-    }
-  }, [])
+      window.removeEventListener("keydown", handleKeyDown);
+    };
+  }, []);
 
   return (
     <div>
       <NumberPad></NumberPad>
     </div>
-  )
-}
+  );
+};
 
-const mapStateToProps = (state) => ({ ...state })
-const mapDispatchToProps = {}
-connect(mapStateToProps, mapDispatchToProps)(Calculator)
+const mapStateToProps = (state) => ({ ...state });
+const mapDispatchToProps = {};
+connect(mapStateToProps, mapDispatchToProps)(Calculator);
 
 ReactDOM.render(
   <Provider store={store}>
