b283575bfb8e1221b03c86456e02f54ad515a3ca
Dracocosmos <notReal@gmail.com>
2024-07-11 11:04:03 +0300

    JavaScript Calc calculates into console.

--- src/JavaScript Calculator/JavaScript Calculator.jsx
+++ src/JavaScript Calculator/JavaScript Calculator.jsx
@@ -15,7 +15,7 @@ import Reset from "../Reset";
 
 const storeInitial = {
   currentTotal: 0,
-  currentFormula: "",
+  currentFormula: [],
   acceptableButtons: {
     "0": { type: "number", id: "zero" },
     "1": { type: "number", id: "one" },
@@ -29,48 +29,110 @@ const storeInitial = {
     "9": { type: "number", id: "nine" },
     ".": { type: "decimal point", id: "dot" },
     ",": { type: "alias", id: "comma", aliasFor: "." },
-    "Enter": { type: "operator", id: "enter" },
     "/": { type: "operator", id: "division" },
     "*": { type: "operator", id: "multiplication" },
     "-": { type: "operator", id: "subtraction" },
     "+": { type: "operator", id: "addition" },
+    "Enter": { type: "enter", id: "enter" },
+    "Backspace": { type: "backspace", id: "backspace" }
   },
 };
 
 const calculatorReducer = (state = storeInitial, action) => {
   switch (action.type) {
     case 'currentFormula/update':
+      // all possible buttons
       const buttons = state.acceptableButtons;
-      const currentButton = buttons[action.payload];
-      const currentFormula = state.currentFormula;
-      const lastInput = currentFormula.slice(-1);
-      const lastInputObject = buttons[lastInput];
+      // a button object
+      let currentButton = buttons[action.payload];
 
       // if action is an alias
       if (currentButton.type === "alias") {
         action.payload = currentButton.aliasFor;
+        currentButton = buttons[currentButton.aliasFor];
       };
 
-      // if action is a repeat operator
-      if (lastInput
-        && lastInputObject.type === "operator"
-        && currentButton.type === "operator"
-      ) {
-        action.payload = "";
-      };
+      // formula as it is, in an array of blocks
+      const formula = state.currentFormula;
+      let formulaCurrentBlock = formula.length !== 0 ?
+        formula[formula.length - 1]
+        : null;
 
-      const newFormula = state.currentFormula + action.payload;
-      const newTotal = newFormula;
+      // the last char in the formula
+      const lastInput = formulaCurrentBlock ?
+        formulaCurrentBlock.slice(-1)
+        : null;
+      // a button object
+      const lastInputObject = lastInput ?
+        buttons[lastInput.at(-1)]
+        : null;
 
-      console.log(newFormula)
+      // TODO: still deletes whole block if last char is . when pressing an operator
+      switch (currentButton.type) {
+        // if action is an operator
+        case "operator":
+          // is a repeat operator
+          if (lastInput
+            && (lastInputObject.type === "operator"
+              || lastInputObject.type === "decimal point")
+          ) {
+            formula[formula.length - 1] = action.payload;
+          } else {
+            formula.push(action.payload);
+          };
+          break;
+
+        // if action is a number
+        case "number":
+          // is a repeat number
+          if (lastInput
+            && (lastInputObject.type === "number"
+              || lastInputObject.type === "decimal point")
+          ) {
+            formula[formula.length - 1] = formulaCurrentBlock + action.payload;
+          } else {
+            formula.push(action.payload);
+          };
+          break;
+
+        case "decimal point":
+          // is not a repeat point
+          if (lastInput
+            && !formulaCurrentBlock.includes(".")
+          ) {
+            // previous is an operator
+            if (lastInputObject.type !== "operator") {
+              formula[formula.length - 1] = formulaCurrentBlock + action.payload;
+            };
+          };
+          break;
+
+        case "backspace":
+          if (lastInput) {
+            formula[formula.length - 1] = formula[formula.length - 1].slice(0, -1);
+            // if empty
+            if (formula[formula.length - 1].length === 0) {
+              formula.pop();
+            };
+          };
+          break;
+
+        default:
+          break;
+      }
+
+      console.log(formula)
       return {
         ...state,
-        currentFormula: newFormula
+        currentFormula: formula
       };
     case 'formula/calculate':
+      //TODO: make string valid, even if it has operator at end, 00 at start etc
+      const formulaString = state.currentFormula.join("");
+      console.log(eval(formulaString))
       return {
         ...state,
-        currentTotal: String.eval
+        currentTotal: eval(formulaString)
       };
     default:
       return state;
@@ -138,6 +200,7 @@ const Calculator = (props) => {
   // calculator button hits
   const handleKeyDown = (event) => {
     event.preventDefault();
+    // console.log(event)
     userInput(event.key);
   };
 
