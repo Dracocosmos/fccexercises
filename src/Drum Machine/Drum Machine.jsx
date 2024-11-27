6476576cc1f4dc7ccedee09e739f3e52d3885308
dracocosmos <124890142+Dracocosmos@users.noreply.github.com>
2024-11-27 08:35:23 +0200

    drum machine passes tests. audio issue remains

--- src/Drum Machine/Drum Machine.jsx
+++ src/Drum Machine/Drum Machine.jsx
@@ -2,7 +2,7 @@ import ReactDOM from "react-dom";
 import React from "react";
 
 import { createStore } from "redux";
-import { Provider } from "react-redux";
+import { Provider, connect } from "react-redux";
 
 import $, { post } from "jquery";
 
@@ -14,44 +14,45 @@ import Reset from "../Reset";
 const storeInitial = {
   samples: [
     {
-      key: 'Q', name: 'Heater 1',
+      key: 'Q', name: 'Heater 1', reactKey: 1,
       url: 'https://cdn.freecodecamp.org/testable-projects-fcc/audio/Heater-1.mp3'
     },
     {
-      key: 'W', name: 'Heater 2',
+      key: 'W', name: 'Heater 2', reactKey: 2,
       url: 'https://cdn.freecodecamp.org/testable-projects-fcc/audio/Heater-2.mp3'
     },
     {
-      key: 'E', name: 'Heater 3',
+      key: 'E', name: 'Heater 3', reactKey: 3,
       url: 'https://cdn.freecodecamp.org/testable-projects-fcc/audio/Heater-3.mp3'
     },
     {
-      key: 'A', name: 'Heater 4',
+      key: 'A', name: 'Heater 4', reactKey: 4,
       url: 'https://cdn.freecodecamp.org/testable-projects-fcc/audio/Heater-4_1.mp3'
     },
     {
-      key: 'S', name: 'Clap',
+      key: 'S', name: 'Clap', reactKey: 5,
       url: 'https://cdn.freecodecamp.org/testable-projects-fcc/audio/Heater-6.mp3'
     },
     {
-      key: 'D', name: 'Open HH',
+      key: 'D', name: 'Open HH', reactKey: 6,
       url: 'https://cdn.freecodecamp.org/testable-projects-fcc/audio/Dsc_Oh.mp3'
     },
     {
-      key: 'Z', name: 'Kick-n\'-hat',
+      key: 'Z', name: 'Kick-n\'-hat', reactKey: 7,
       url: 'https://cdn.freecodecamp.org/testable-projects-fcc/audio/Kick_n_Hat.mp3'
     },
     {
-      key: 'X', name: 'Kick',
+      key: 'X', name: 'Kick', reactKey: 8,
       url: 'https://cdn.freecodecamp.org/testable-projects-fcc/audio/RP4_KICK_1.mp3'
     },
     {
-      key: 'C', name: 'Closed-HH',
+      key: 'C', name: 'Closed-HH', reactKey: 9,
       url: 'https://cdn.freecodecamp.org/testable-projects-fcc/audio/Cev_H2.mp3'
     }
   ],
-  lastPlayed: null
+  lastPlayed: null,
 };
+
 
 const audioReducer = (state = storeInitial, action) => {
   switch (action.type) {
@@ -76,49 +77,72 @@ class DrumButtons extends React.Component {
 
     // remember to bind this if you make any methods inside object
     this.playSound = this.playSound.bind(this);
+    this.keyDown = this.keyDown.bind(this);
+
   }
+
+  keyDown(event) {
+    this.state.samples.forEach((sample) => {
+      if (event.key.toLowerCase() == sample.key.toLowerCase()) {
+        this.playSound(event, sample)
+      };
+    });
+  };
 
   playSound(event, sample) {
     event.preventDefault();
 
-    let audio = new Audio(sample.url);
+    let audio = $(`#${sample.key}`)[0];
     audio.play();
 
     store.dispatch({ type: "lastPlayed/update", payload: sample })
   }
 
-  componentDidMount() {
-    const parent = document.getElementById("drum-machine");
+  // TODO: something is too slow with playing keys, maybe have to cancel prev audio to play next
 
-    if (parent) {
-      parent.childNodes.forEach((element, index) => {
-        element.insertAdjacentElement('afterend', (<p>hello</p>))
-      });
-    };
+  componentDidMount() {
+    document.addEventListener("keydown", this.keyDown);
+  };
+
+  componentWillUnmount() {
+    document.removeEventListener("keydown", this.keyDown);
   }
 
   render() {
     return (
       // <> = react fragment, jsx requires a parent element.
       <>
-        {this.state.samples.map((sample, index) => {
+        {this.state.samples.map((sample, _index) => {
           return (
-            <button
-              id={`${sample.name}-button`}
-              key={index}
-              // when clicked, send the sample object to playsound
-              onClick={(event) => this.playSound(event, sample)}
-              className="drum-pad"
-              type="button"
+            <div
+              className="button-wrapper"
+              key={`${sample.reactKey}-fragment`}
             >
-              <audio
-                src={sample.url}
-                id={sample.key}
-                className="clip"
+              <button
+                id={`${sample.name}-button`}
+                key={`${sample.reactKey}-button`}
+                // when clicked, send the sample object to playsound
+                onClick={(event) => this.playSound(event, sample)}
+                className="drum-pad"
+                type="button"
               >
-              </audio>
-              {sample.key}
-            </button>
+                <audio
+                  src={sample.url}
+                  id={sample.key}
+                  className="clip"
+                  key={`${sample.reactKey}-audio`}
+                >
+                </audio>
+                {sample.key}
+              </button>
+              <div
+                id={`${sample.name}-div`}
+                key={`${sample.reactKey}-div`}
+                className="under-button"
+              >
+                {sample.name}
+              </div>
+            </div>
           );
         })}
       </>
@@ -132,14 +156,24 @@ class DrumDisplay extends React.Component {
   }
 
   render() {
+    const lastPlayed = this.props.lastPlayed
     return (
       <div
         id="display"
+        key="display-div"
       >
+        {lastPlayed ? lastPlayed.name : ""}
       </div>
     )
   }
 };
+
+// this connects the area to the store,
+// so that it can receive updates
+const mapStateToProps = (state, _ownprops) => {
+  return { lastPlayed: state.lastPlayed }
+};
+DrumDisplay = connect(mapStateToProps)(DrumDisplay);
 
 // wrapper div for buttons
 class DrumMachine extends React.Component {
