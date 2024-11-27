2297db5e70a868ace9bb250b8a6eb7eddef35f86
dracocosmos <124890142+Dracocosmos@users.noreply.github.com>
2024-11-27 08:35:29 +0200

    fixandcreatetables.sql commented, added loop for adding rows to properties.type_id

--- RelationalDatabases/pertabledb/fixandcreatetables.sql
+++ RelationalDatabases/pertabledb/fixandcreatetables.sql
@@ -1,3 +1,4 @@
+-- rename and set constraints in properties
 ALTER TABLE properties 
   RENAME COLUMN weight TO atomic_mass;
 ALTER TABLE properties 
@@ -8,44 +9,69 @@ ALTER TABLE properties
   ALTER COLUMN melting_point_celsius SET NOT NULL,
   ALTER COLUMN boiling_point_celsius SET NOT NULL;
 
+-- set elements table constraints
 ALTER TABLE elements 
   ADD UNIQUE (name),
   ADD UNIQUE (symbol),
   ALTER COLUMN name SET NOT NULL,
   ALTER COLUMN symbol SET NOT NULL;
 
+-- set foreign key in properties.atomic_number
 ALTER TABLE properties
   ADD FOREIGN KEY (atomic_number) REFERENCES elements(atomic_number);
 
+-- remove false entry
 DELETE FROM properties
   WHERE atomic_number = 1000;
-
 DELETE FROM elements 
   WHERE atomic_number = 1000;
 
+-- create new types table
 CREATE TABLE types (
   type_id SERIAL PRIMARY KEY,
   type VARCHAR(25) NOT NULL
 );
 
+-- get rows for types table
 INSERT INTO types (
   type
 )
 SELECT DISTINCT type 
   FROM properties;
 
+-- set properties.type_id foreign key
 ALTER TABLE properties
   ADD COLUMN type_id INT,
   ADD FOREIGN KEY (type_id) REFERENCES types(type_id);
 
--- add the entries, then make not null
-UPDATE properties 
-  SET type_id = 
-  WHERE;
+-- add the entries to properties.type_id
+-- from new types table
+DO
+$$
+DECLARE
+  t record;
+BEGIN
+  FOR t IN SELECT type, type_id
+    FROM types
+  LOOP
+    UPDATE properties
+      SET type_id = (
+        SELECT type_id
+        FROM types
+        WHERE type_id = t.type_id
+      )
+      WHERE type = t.type;
+  END LOOP;
+END;
+$$;
 
+-- set properties.type_id to null
 ALTER TABLE properties 
-  ALTER COLUMN type_id SET NOT NULL,
+  ALTER COLUMN type_id SET NOT NULL;
 
--- do last
+-- remove type column from properties
 ALTER TABLE properties
   DROP COLUMN type;
+
+
+
