2a0d123f176a5b76d1a0dc1a490f670be822f60a
dracocosmos <124890142+Dracocosmos@users.noreply.github.com>
2024-11-27 08:35:28 +0200

    D3 Tree Map separated drawing svg from main function, added buttons for changing data.

--- src/D3 Tree Map/D3 Tree Map.js
+++ src/D3 Tree Map/D3 Tree Map.js
@@ -16,15 +16,87 @@ import $ from "jquery";
 
 import * as d3 from "d3"
 
+// values
+const width = 1000,
+  height = 600,
+  margin = 30,
+  fullWidth = width + margin * 2
+
+const drawSvg = (data) => {
+  const dataEntries = data.children
+  console.log(data)
+
+
+  // svg container
+  const svg = d3.select("#root")
+    .append("svg")
+    .attr("width", width + margin * 2)
+    .attr("height", height + margin * 2)
+    .attr("id", "svg-container")
+
+  // background
+  svg.append("rect")
+    .attr("width", "100%")
+    .attr("height", "100%")
+    .attr("fill", "DarkSeaGreen")
+    .attr("id", 'svg-background')
+
+  // title
+  svg.append('text')
+    .text(data.name)
+    .attr('x', 100)
+    .attr('y', margin / 2 + 10)
+    .attr("id", "title")
+
+  // description 
+  svg.append('text')
+    .text(`I describe things`)
+    .attr('x', 20)
+    .attr('y', height + margin * 2 - 10)
+    .attr("id", "description")
+
+  // legend
+}
 
 const main = (kickstarterData, movieData, gameData) => {
+
   console.log(kickstarterData)
   console.log(movieData)
   console.log(gameData)
 
-  svg = d3.select('root')
-    .append()
+  kickstarterData['button-name'] = 'Kickstarter'
+  movieData['button-name'] = 'Movies'
+  gameData['button-name'] = 'Games'
 
+  // create svg change buttons
+  const rootE = $('#root')
+
+  const buttonDiv = $(document.createElement('div'))
+    .attr('id', 'button-wrapper')
+    .css('width', fullWidth)
+    .appendTo(rootE);
+
+  // on pressing a button
+  const buttonClick = (e) => {
+    e.preventDefault()
+
+    // TODO: clear svg
+
+    //create new svg
+    drawSvg(e.data)
+  }
+
+  // button for every data source
+  [kickstarterData, movieData, gameData].forEach((data) => {
+    buttonDiv.append($(document.createElement('button'))
+      .attr('id', data['button-name'].toLowerCase() + '-button')
+      .text(data['button-name'])
+      .on('click', data, buttonClick)
+    )
+  })
+
+  // choose which svg to draw by default
+  drawSvg(gameData)
 }
 
 // fetches data from session storage,
